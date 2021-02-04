df05401f6f998addd78469e09b073579887d7ca9
Satish Kumar <satish.kumar06@nagarro.com>
2021-02-04 12:56:11 +0530

    Set-up template engine
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello from server")
+    res.render('home')
 })
+
+// Set Template Engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
