b0654fc6748745af720f2a501cb87a80fad1ebbf
Satish Kumar <satish.kumar06@nagarro.com>
2021-02-07 14:03:32 +0530

    Build the empty cart section
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- server.js
+++ server.js
@@ -10,14 +10,18 @@ const PORT = process.env.PORT || 3000
 //Assets
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
 // Set Template Engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
