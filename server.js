b7d903208c05708f39803e029f2980e3535413cc
Satish Kumar <satish.kumar06@nagarro.com>
2021-02-15 11:47:38 +0530

    Build add to cart functionality
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- server.js
+++ server.js
@@ -43,6 +43,14 @@ app.use(flash())
 //Assets
 app.use(express.static('public'))
 
+app.use(express.json())
+
+// Global middleware 
+app.use((req, res, next) => {
+  res.locals.session = req.session
+  next()
+})
+
 // Set Template Engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
