93b6124b680fe5928b02d419c4c1bff259145ef2
Satish Kumar <satish.kumar06@nagarro.com>
2021-02-23 16:07:17 +0530

    Building Place an order functionality and custumer-order page
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,11 @@
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const orderController = require('../app/http/controllers/customers/orderController')
 
 // Middlewares 
 const guest = require('../app/http/middlewares/guest')
+const auth = require('../app/http/middlewares/auth')
 
 function initRoutes(app) {
 
@@ -23,6 +25,10 @@ function initRoutes(app) {
 
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update)
+
+    // Customer routes
+    app.post('/orders', auth, orderController().store)
+    app.get('/customer/orders', auth, orderController().index)
       
 }
 
